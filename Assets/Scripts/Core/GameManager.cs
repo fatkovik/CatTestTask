2390ea8a8589318a049e46ae1138807870c4ccea
fatkovik <ghazariandavid@gmail.com>
2026-02-26 18:18:18 +0400

    Lives added, along with UI. wrong clicks affect lives

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,16 +1,38 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text livesCounter;
     [SerializeField] private int totalLives;
+    [SerializeField] private CatSpotManager catSpotManager;
+
+    private int _currentLives;
+
     void Start()
     {
-        
+        _currentLives = totalLives;
+        catSpotManager.onWrongSpotClicked += OnLifeLost;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLifeLost()
     {
-        
+        _currentLives--;
+        SetLivesText();
+
+        if (_currentLives <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+
+    private void SetLivesText()
+    {
+        livesCounter.text = $"{_currentLives}";
+    }
+
+    private void OnDisable()
+    {
+        catSpotManager.onWrongSpotClicked -= OnLifeLost;
     }
 }
--- Assets/Scripts/Gameplay/CatSpotManager.cs
+++ Assets/Scripts/Gameplay/CatSpotManager.cs
@@ -1,13 +1,16 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatSpotManager : MonoBehaviour
 {
-    [SerializeField] TMP_Text tmpCounter;
+    [SerializeField] TMP_Text foundCatsCounter;
+    [SerializeField] Button mapBackground;
+
+    internal Action onWrongSpotClicked;
 
     private List<CatSpot> allCats;
     private int catsClickedCount = 0;
@@ -21,6 +24,8 @@ public class CatSpotManager : MonoBehaviour
 
         totalCatsCount = allCats.Count;
         SetCounterText();
+
+        mapBackground.onClick.AddListener(OnWrongSpotClicked);
     }
 
     private void OnCatClicked(CatSpot catspot)
@@ -35,13 +40,19 @@ public class CatSpotManager : MonoBehaviour
         SetCounterText();
     }
 
+    private void OnWrongSpotClicked()
+    {
+        onWrongSpotClicked?.Invoke();
+    }
+
     private void SetCounterText()
     {
-        tmpCounter.text = $"{catsClickedCount}/{totalCatsCount}";
+        foundCatsCounter.text = $"{catsClickedCount}/{totalCatsCount}";
     }
 
     private void OnDisable()
     {
         allCats.ForEach(cat => cat.catClicked -= OnCatClicked);
+        mapBackground.onClick.RemoveListener(OnWrongSpotClicked);
     }
 }
